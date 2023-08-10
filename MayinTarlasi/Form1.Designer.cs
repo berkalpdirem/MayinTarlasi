27364664b1278649f5ee7e4ab333ac9cea07b2e9
Berk Alp Direm <berkalpdirem@hotmail.com>
2023-08-11 00:21:18 +0300

    Add project files.

---
+++ MayinTarlasi/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace MayinTarlasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl_buttonPanel = new System.Windows.Forms.Panel();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.tb_MineCount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // pnl_buttonPanel
+            // 
+            this.pnl_buttonPanel.Location = new System.Drawing.Point(34, 33);
+            this.pnl_buttonPanel.Name = "pnl_buttonPanel";
+            this.pnl_buttonPanel.Size = new System.Drawing.Size(410, 330);
+            this.pnl_buttonPanel.TabIndex = 0;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(478, 59);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 1;
+            this.btn_Start.Text = "Başla";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // tb_MineCount
+            // 
+            this.tb_MineCount.Location = new System.Drawing.Point(478, 33);
+            this.tb_MineCount.Name = "tb_MineCount";
+            this.tb_MineCount.Size = new System.Drawing.Size(100, 20);
+            this.tb_MineCount.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tb_MineCount);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.pnl_buttonPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_buttonPanel;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.TextBox tb_MineCount;
+    }
+}
+
---
+++ MayinTarlasi/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayinTarlasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<field> fieldList = new List<field>();
+        List<mine> mineList = new List<mine>();
+
+        public class field
+        {
+            public int x { get; set; }
+
+            public int y { get; set; }
+
+            public bool IsItBomb { get; set; }
+
+            public Button button { get; set; }
+        }
+        
+        public class mine
+        {
+            
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (true)
+            {
+                MessageBox.Show("Patladı");
+            }
+            else
+            {
+                MessageBox.Show("Temiz");
+            }
+            
+        }
+
+        
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            pnl_buttonPanel.Controls.Clear();
+            fieldList.Clear();
+
+            //defult olarak 5 ayın var
+            int mineCount = 5;
+            if (tb_MineCount.Text == string.Empty)
+            {
+                mineCount = 5;
+                
+            }
+            else
+            {
+                mineCount = int.Parse(tb_MineCount.Text);
+            }
+            
+            
+            
+            //Mine Count kadar mine oluşturukuyor
+            for (int i = 0; i < mineCount; i++)
+            {
+                mine _mine = new mine();
+                mineList.Add(_mine);
+            }
+
+            // Creating Buttons and field Objects
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    field _field = new field();
+                    _field.x = x;
+                    _field.y = y;
+
+                    Button btn = new Button();
+
+                    pnl_buttonPanel.Controls.Add(btn);
+                    btn.Size = new Size(20, 20);
+                    btn.Location = new Point(20 * x, 20 * y);
+                    btn.Click += new EventHandler(button_Click);
+                    _field.button = btn;
+
+                    fieldList.Add(_field);
+                }
+            }
+
+            Random rnd = new Random();
+            List<int> templist = new List<int>();
+            for (int i = 0; i < mineCount; i++)
+            {
+                int temp = rnd.Next(0, 100);
+                if (!templist.Contains(temp))
+                {
+                    templist.Add(temp);
+                    fieldList[temp].button.BackColor = Color.Red;
+                }
+                else
+                {
+                    i--;
+                }
+
+            }
+            
+            
+        }
+    }
+}
