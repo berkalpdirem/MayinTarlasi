eb1301b41e8b34e88dcae14a0421ce7bebff6630
Berk Alp Direm <berkalpdirem@hotmail.com>
2023-08-12 01:37:43 +0300

    Created playing methods

--- MayinTarlasi/Form1.Designer.cs
+++ MayinTarlasi/Form1.Designer.cs
@@ -28,45 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pnl_buttonPanel = new System.Windows.Forms.Panel();
             this.btn_Start = new System.Windows.Forms.Button();
             this.tb_MineCount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDownMin = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownSecond = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTimeWarning = new System.Windows.Forms.Label();
+            this.lbl_Min = new System.Windows.Forms.Label();
+            this.lbl_Sec = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.gbTimeWarning = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSecond)).BeginInit();
+            this.gbTimeWarning.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnl_buttonPanel
             // 
             this.pnl_buttonPanel.Location = new System.Drawing.Point(34, 33);
             this.pnl_buttonPanel.Name = "pnl_buttonPanel";
-            this.pnl_buttonPanel.Size = new System.Drawing.Size(410, 330);
+            this.pnl_buttonPanel.Size = new System.Drawing.Size(500, 500);
             this.pnl_buttonPanel.TabIndex = 0;
             // 
             // btn_Start
             // 
-            this.btn_Start.Location = new System.Drawing.Point(478, 59);
+            this.btn_Start.Location = new System.Drawing.Point(713, 91);
             this.btn_Start.Name = "btn_Start";
             this.btn_Start.Size = new System.Drawing.Size(75, 23);
             this.btn_Start.TabIndex = 1;
             this.btn_Start.Text = "Başla";
             this.btn_Start.UseVisualStyleBackColor = true;
-            this.btn_Start.Click += new System.EventHandler(this.buttonStart_Click);
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
             // 
             // tb_MineCount
             // 
-            this.tb_MineCount.Location = new System.Drawing.Point(478, 33);
+            this.tb_MineCount.Location = new System.Drawing.Point(713, 32);
             this.tb_MineCount.Name = "tb_MineCount";
-            this.tb_MineCount.Size = new System.Drawing.Size(100, 20);
+            this.tb_MineCount.Size = new System.Drawing.Size(75, 20);
             this.tb_MineCount.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(639, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mayın Sayısı:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(573, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Oyun Süresi:";
+            // 
+            // numericUpDownMin
+            // 
+            this.numericUpDownMin.Location = new System.Drawing.Point(642, 62);
+            this.numericUpDownMin.Maximum = new decimal(new int[] {
+            -469762049,
+            -590869294,
+            5421010,
+            0});
+            this.numericUpDownMin.Minimum = new decimal(new int[] {
+            1241513983,
+            370409800,
+            542101,
+            -2147483648});
+            this.numericUpDownMin.Name = "numericUpDownMin";
+            this.numericUpDownMin.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDownMin.TabIndex = 6;
+            // 
+            // numericUpDownSecond
+            // 
+            this.numericUpDownSecond.Location = new System.Drawing.Point(713, 62);
+            this.numericUpDownSecond.Maximum = new decimal(new int[] {
+            268435455,
+            1042612833,
+            542101086,
+            0});
+            this.numericUpDownSecond.Minimum = new decimal(new int[] {
+            268435455,
+            1042612833,
+            542101086,
+            -2147483648});
+            this.numericUpDownSecond.Name = "numericUpDownSecond";
+            this.numericUpDownSecond.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDownSecond.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(685, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "DK";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(756, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "SN";
+            // 
+            // lblTimeWarning
+            // 
+            this.lblTimeWarning.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimeWarning.Location = new System.Drawing.Point(11, 36);
+            this.lblTimeWarning.Name = "lblTimeWarning";
+            this.lblTimeWarning.Size = new System.Drawing.Size(167, 46);
+            this.lblTimeWarning.TabIndex = 7;
+            this.lblTimeWarning.Text = "KalanSüre:";
+            // 
+            // lbl_Min
+            // 
+            this.lbl_Min.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Min.Location = new System.Drawing.Point(6, 82);
+            this.lbl_Min.Name = "lbl_Min";
+            this.lbl_Min.Size = new System.Drawing.Size(48, 46);
+            this.lbl_Min.TabIndex = 7;
+            this.lbl_Min.Text = "00";
+            // 
+            // lbl_Sec
+            // 
+            this.lbl_Sec.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Sec.Location = new System.Drawing.Point(106, 82);
+            this.lbl_Sec.Name = "lbl_Sec";
+            this.lbl_Sec.Size = new System.Drawing.Size(52, 46);
+            this.lbl_Sec.TabIndex = 7;
+            this.lbl_Sec.Text = "00";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(53, 82);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 46);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "dk:";
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(158, 82);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 46);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "sn";
+            // 
+            // gbTimeWarning
+            // 
+            this.gbTimeWarning.Controls.Add(this.lblTimeWarning);
+            this.gbTimeWarning.Controls.Add(this.lbl_Sec);
+            this.gbTimeWarning.Controls.Add(this.lbl_Min);
+            this.gbTimeWarning.Controls.Add(this.label9);
+            this.gbTimeWarning.Controls.Add(this.label8);
+            this.gbTimeWarning.Location = new System.Drawing.Point(549, 236);
+            this.gbTimeWarning.Name = "gbTimeWarning";
+            this.gbTimeWarning.Size = new System.Drawing.Size(229, 149);
+            this.gbTimeWarning.TabIndex = 8;
+            this.gbTimeWarning.TabStop = false;
+            this.gbTimeWarning.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 539);
+            this.Controls.Add(this.gbTimeWarning);
+            this.Controls.Add(this.numericUpDownSecond);
+            this.Controls.Add(this.numericUpDownMin);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.tb_MineCount);
             this.Controls.Add(this.btn_Start);
             this.Controls.Add(this.pnl_buttonPanel);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSecond)).EndInit();
+            this.gbTimeWarning.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,6 +239,19 @@
         private System.Windows.Forms.Panel pnl_buttonPanel;
         private System.Windows.Forms.Button btn_Start;
         private System.Windows.Forms.TextBox tb_MineCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDownMin;
+        private System.Windows.Forms.NumericUpDown numericUpDownSecond;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTimeWarning;
+        private System.Windows.Forms.Label lbl_Min;
+        private System.Windows.Forms.Label lbl_Sec;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.GroupBox gbTimeWarning;
     }
 }
 
--- MayinTarlasi/Form1.cs
+++ MayinTarlasi/Form1.cs
@@ -7,121 +7,243 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net.Http;
+
 
 
 namespace MayinTarlasi
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-        }
-        List<field> fieldList = new List<field>();
-        List<mine> mineList = new List<mine>();
 
-        public class field
-        {
-            public int x { get; set; }
-
-            public int y { get; set; }
-
-            public bool IsItBomb { get; set; }
-
-            public Button button { get; set; }
-        }
-        
-        public class mine
-        {
-            
         }
 
-        private void button_Click(object sender, EventArgs e)
+        field[,] GameMatrix = new field[10, 10];
+        List<(int, int)> MineCoordinates = new List<(int, int)>();
+        int min;
+        int sec;
+
+        //--------------------------------------------------------
+        #region Methods
+        /// <summary>
+        /// Scans the surrounding fields of the selected region 
+        /// </summary>
+        /// <param name="RelatedButton"></param>
+        public void ScanFields(Button RelatedButton)
         {
-            Button btn = sender as Button;
-            field _field =btn.Tag as field;
-            if (_field.IsItBomb)
+            List<field> neighborsList = new List<field>();
+
+            field selectedField = RelatedButton.Tag as field;
+            int xDownBorder = selectedField.x - 1;
+            int xUpBorder = selectedField.x + 1;
+
+            int yDownBorder = selectedField.y - 1;
+            int yUpBorder = selectedField.y + 1;
+
+            int count = 0;
+            for (int y = yDownBorder; y <= yUpBorder; y++)
             {
-                MessageBox.Show("Boom \n" + (_field.x + " " + _field.y));
-                
-                
+
+                for (int x = xDownBorder; x <= xUpBorder; x++)
+                {
+                    if (x != -1 && x != 10 && y != -1 && y != 10 && GameMatrix[x, y].IsItBomb)
+                    {
+                        count++;
+                        selectedField.button.Text = count.ToString();
+                    }
+
+                }
             }
-            else
-            {
-                MessageBox.Show("Temiz");
-            }
-            
         }
 
-        
-        private void buttonStart_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Shows the location of the mines after the game is over 
+        /// </summary>
+        public void ShowMines()
         {
-            pnl_buttonPanel.Controls.Clear();
-            fieldList.Clear();
+            for (int i = 0; i < MineCoordinates.Count; i++)
+            {
+                (int tempX, int tempY) = MineCoordinates[i];
+                GameMatrix[tempX, tempY].button.BackColor = Color.Red;
+            }
+        }
 
-            //defult olarak 5 ayın var
-            int mineCount = 5;
-            if (tb_MineCount.Text == string.Empty)
-            {
-                mineCount = 5;
-            }
-            else
-            {
-                mineCount = int.Parse(tb_MineCount.Text);
-            }
-            
-            
-            
-            //Mine Count kadar mine oluşturukuyor
-            for (int i = 0; i < mineCount; i++)
-            {
-                mine _mine = new mine();
-                mineList.Add(_mine);
-            }
-
+        /// <summary>
+        /// Creating Game Panel and seting mines
+        /// </summary>
+        /// <param name="mineCount"></param>
+        public void StartGame(int mineCount)
+        {
             // Creating Buttons and field Objects
             for (int y = 0; y < 10; y++)
             {
                 for (int x = 0; x < 10; x++)
                 {
+                    //Creating Fields
                     field _field = new field();
                     _field.x = x;
                     _field.y = y;
 
+                    //Creating Buttons
                     Button btn = new Button();
-
                     pnl_buttonPanel.Controls.Add(btn);
-                    btn.Size = new Size(20, 20);
-                    btn.Location = new Point(20 * x, 20 * y);
+                    btn.Size = new Size(50, 50);
+                    btn.Location = new Point(50 * x, 50 * y);
                     btn.Click += new EventHandler(button_Click);
                     btn.Tag = _field;
                     _field.button = btn;
+                    //btn.Text = x.ToString() + " " + y.ToString();
 
-                    fieldList.Add(_field);
+                    // Creating GameMatrix
+                    GameMatrix[x, y] = _field;
                 }
             }
 
+            // Add mines to fields
             Random rnd = new Random();
-            List<int> templist = new List<int>();
+
+
             for (int i = 0; i < mineCount; i++)
             {
-                int temp = rnd.Next(0, 100);
-                if (!templist.Contains(temp))
-                {
-                    templist.Add(temp);
-                    fieldList[temp].IsItBomb = true;
-                    ((field)(fieldList[temp].button.Tag)).IsItBomb = true;
+                int tempX = rnd.Next(0, 10);
+                int tempY = rnd.Next(0, 10);
+                (int, int) tempTuple = (tempX, tempY);
 
-                    fieldList[temp].button.BackColor = Color.Red;
+                if (!MineCoordinates.Contains(tempTuple))
+                {
+                    MineCoordinates.Add(tempTuple);
+                    GameMatrix[tempX, tempY].IsItBomb = true;
+                    GameMatrix[tempX, tempY].button.BackColor = Color.Black;
+
                 }
                 else
                 {
                     i--;
                 }
+            }
+
+        }
+        #endregion
+
+
+        //---------------------------------------------------------
+        #region Button Events
+        /// <summary>
+        /// GamePanel's buttons behavior
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            field _field = btn.Tag as field;
+            if (_field.IsItBomb)
+            {
+                MessageBox.Show("Boom");
+                ShowMines();
+            }
+            else
+            {
+                btn.BackColor = Color.Green;
+                ScanFields(btn);
 
             }
+
+        }
+
+        /// <summary>
+        /// Get MineCount and Start Game
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            pnl_buttonPanel.Controls.Clear();
+            MineCoordinates.Clear();
+            gbTimeWarning.Visible = false;
+            min = 0;
+            sec = 0;
+            if (int.TryParse(tb_MineCount.Text, out int mineCount) && int.Parse(tb_MineCount.Text) < 101)
+            {
+                if ((numericUpDownMin.Value < 0 && numericUpDownSecond.Value <= 1))
+                {
+                    MessageBox.Show("Lütfen Oynayacağınız Süreyi Dakika ve Saniye Cinsinden Giriniz !!!");
+                }
+                else
+                {
+                    min = Convert.ToInt32(numericUpDownMin.Value);
+                    sec = Convert.ToInt32(numericUpDownSecond.Value);
+                    mineCount = int.Parse(tb_MineCount.Text);
+                    
+                    timer1.Start();
+                    StartGame(mineCount);
+                    gbTimeWarning.Visible = true;
+
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Mayın Sayısına, 0'dan 100'e Kadar Herhangi Bir Sayısal Değer Girin!!!");
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
             
+            if (sec / 60 > 0)
+            {
+                int temp = sec / 60;
+                min = min + temp;
+                sec = sec % 60;
+            }
             
+
+            if (sec == 0 && min == 0)
+            {   
+                timer1.Stop();
+                lbl_Sec.Text = sec.ToString();
+                lbl_Min.Text = min.ToString();
+                MessageBox.Show("Süre Bitti !!!");
+            }
+            if (sec ==0 && min > 0)
+            {
+                min--;
+                sec = sec+60;
+                
+            }
+            lbl_Sec.Text = sec.ToString();
+            lbl_Min.Text = min.ToString();
+            sec -= 1;
+        }
+
+
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
+
+    /// <summary>
+    /// Creating field class for keeping mine information
+    /// </summary>
+    public class field
+    {
+        public int x { get; set; }
+
+        public int y { get; set; }
+
+        public bool IsItBomb { get; set; }
+
+        public Button button { get; set; }
+    }
 }
+
